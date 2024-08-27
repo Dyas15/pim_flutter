08ddf0ed9b510e5686f62e09955bae9fdb2c752c
ryanzlima <silvaryan548@gmail.com>
2024-08-27 17:03:29 -0300

    Código desnecessario removidos

--- lib/home.dart
+++ lib/home.dart
@@ -8,41 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Color> opcoesCorBarra = [
-    Colors.green[900]!,
-    Colors.red[900]!,
-    Colors.blue[900]!,
-    Colors.orange[900]!,
-    Colors.purple[900]!
-  ];
-  
-  List<Color> opcoesCorTexto = [
-    Colors.green[800]!,
-    Colors.red[800]!,
-    Colors.blue[800]!,
-    Colors.orange[800]!,
-    Colors.purple[800]!
-  ];
-  
-  List<Color> opcoesCorFundo = [
-    Colors.green[50]!,
-    Colors.red[50]!,
-    Colors.blue[50]!,
-    Colors.orange[50]!,
-    Colors.purple[50]!
-  ];
-  
-  List<String> cores = [
-    'Verde',
-    'Vermelho',
-    'Azul',
-    'Laranja',
-    'Roxo'
-  ];
-
-  int opcaoAtual = 0;
-
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
