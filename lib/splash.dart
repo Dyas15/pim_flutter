c480ca7b9d92e3981bdd516554525435eda9b0e2
Diogo <98923655+Dyas15@users.noreply.github.com>
2024-09-12 00:36:31 -0300

    Classe tela splash
    Foi criado uma classe aparte para a tela splash para tirar do main

---
+++ lib/splash.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'login.dart';
+
+class TelaSplash extends StatelessWidget {
+  const TelaSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diogo Farms',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TelaSplashInicial(),
+    );
+  }
+}
+
+class TelaSplashInicial extends StatefulWidget {
+  const TelaSplashInicial({super.key});
+
+  @override
+  EstadoTelaSplashInicial createState() => EstadoTelaSplashInicial();
+}
+
+class EstadoTelaSplashInicial extends State<TelaSplashInicial> {
+  double _opacidade = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animarLogo();
+    _navegarParaLogin();
+  }
+
+  void _animarLogo() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _opacidade = 1.0;
+      });
+    });
+  }
+
+  void _navegarParaLogin() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Navigator.of(context).pushReplacement(
+      PageTransition(
+        type: PageTransitionType.fade,
+        child: const Login(),
+        duration: const Duration(milliseconds: 1000),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _opacidade,
+          duration: const Duration(seconds: 1),
+          child: Image.asset('images/logo_pequena.png'),
+        ),
+      ),
+    );
+  }
+}
